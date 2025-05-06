41c173b6262890f7fb552e63bc18f3e86c9de947
Willonlyonexd <houwenvt@gmail.com>
2025-05-05 22:03:29 -0400

    11111

--- start.sh
+++ start.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+
+echo "⏳ Entrenando modelo de recomendación..."
+python modelo_entrenamiento.py
+
+echo "🚀 Iniciando servidor FastAPI..."
 uvicorn app:app --host 0.0.0.0 --port ${PORT}
+gi
